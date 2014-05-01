5c3560c14582a9aa80716be628bc1caedd353ccf
larsonholgers <l.holgers@nhs-inc.com>
2014-05-01 11:02:00 -0700

    editted setup.php

--- setup.php
+++ setup.php
@@ -1,16 +1,16 @@
 <?php
 //adodb
-$path_to_adodb = "";
-if ($path_to_adodb != "") {
+$path_to_adodb = "PATH_TO_ADODB";
+if ($path_to_adodb != "" && $path_to_adodb != "PATH_TO_ADODB") {
 	include($path_to_adodb);
 }
 
 //connections
 $connect = array(
-	"host" => "",
-	"login" => "",
-	"pass" => "",
-	"db" => ""
+	"host" => "HOST",
+	"login" => "LOGIN",
+	"pass" => "PASS",
+	"db" => "DB"
 );
 function newDB() {
 	$conn = &ADONewConnection('mysqlt');
