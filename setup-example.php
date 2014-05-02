8022fa587e3379942417b54024bb0bb24c3ef008
larsonholgers <l.holgers@nhs-inc.com>
2014-05-02 16:47:47 -0700

    Added some notes to setup-example.php

--- setup-example.php
+++ setup-example.php
@@ -1,9 +1,13 @@
 <?php
+//Edit this file and rename it "setup.php"
+
 //adodb
 $path_to_adodb = "PATH_TO_ADODB";
 if ($path_to_adodb != "" && $path_to_adodb != "PATH_TO_ADODB") {
 	include($path_to_adodb);
 }
+
+//database
 function newDB() {
 	$conn = ADONewConnection('mysqlt');
 	if (!$conn->Connect("HOST","LOGIN","PASSWORD","DATABASE")) {
